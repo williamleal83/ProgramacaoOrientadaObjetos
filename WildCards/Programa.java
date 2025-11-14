a68f4f685193ee0d9853da0d1552b97f0cf9f31a
William Leal <willianleal@gmail.com>
2025-11-14 17:59:03 -0300

    Adição do exemplo de Wildcard
    Adição do exemplo de Wildcard

--- WildCards/Programa.java
+++ WildCards/Programa.java
@@ -1,22 +1,49 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Programa {
     public static void main(String[] args) {
-        List<Integer> myInts = Arrays.asList(5, 2, 10);
-        printList(myInts);
+        // Unbounded wildcard
+        List<Integer> listaInteiros = Arrays.asList(5, 2, 10);
+        imprimirLista(listaInteiros);
 
         List<String> myStr = Arrays.asList("Maria", "Joaquina", "Cirilo");
-        printList(myStr);
+        imprimirLista(myStr);
+
+        // Upper bounded wildcard
+        List<Double> listaDoubles = Arrays.asList(1.5, 2.0, 3.7);
+        System.out.println("Soma dos números: " + somarNumeros(listaDoubles));
+
+        // Lower bounded wildcard
+        List<Integer> listaInteiros2 = new ArrayList<>();
+        adicionarInteiros(listaInteiros2);
+        imprimirLista(listaInteiros2);
 
         // List<?> list = new ArrayList<Integer>();
         // list.add(3); // erro de compilação
     }
 
-    public static void printList(List<?> list) {
+    // Unbounded wildcard
+    public static void imprimirLista(List<?> list) {
         // list.add(3); Erro de compilação, o compilador não sabe qual é o tipo
         for (Object obj : list) {
             System.out.println(obj);
         }
+    }
+
+    // Upper bounded wildcard
+    public static double somarNumeros(List<? extends Number> lista) {
+        double soma = 0;
+        for (Number n : lista) {
+            soma += n.doubleValue();
+        }
+        return soma;
+    }
+
+    // Lower bounded wildcard
+    public static void adicionarInteiros(List<? super Integer> lista) {
+        lista.add(10);
+        lista.add(20);
     }
 }
